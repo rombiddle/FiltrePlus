5bdec952124ed19f41fef3e3ae503596a0d47607
Romain Brunie <romainbrunie@mbp-de-romain.home>
2017-07-14 16:39:41 +0200

    Romain a pushé

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,13 +1,12 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-#include "imagemanager.h"
-#include "niveaugris.h"
 #include <QImageWriter>
 #include <QFileDialog>
 #include "opencv2/imgproc.hpp"
 #include "opencv2/imgcodecs.hpp"
 #include "opencv2/highgui.hpp"
 #include "opencv2/core.hpp"
+#include <iostream>
 
 
 MainWindow::MainWindow(QWidget *parent) :
@@ -24,33 +23,44 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_addImageButton_clicked()
 {
-    this->fileName = QFileDialog::getOpenFileName(this,tr("Open Image"),QDir::currentPath(),tr("Image Files [ *.jpg , *.jpeg , *.bmp , *.png , *.gif]"));
-    this->manage = ImageManager::ImageManager(fileName);
-    this->currentImage = manage.LoadImage();
+    fileName = QFileDialog::getOpenFileName(this,tr("Open Image"),QDir::currentPath(),tr("Image Files [ *.jpg , *.jpeg , *.bmp , *.png , *.gif]"));
 
-    /**
     charFileName = fileName.toLocal8Bit().data();
+
     iplImg = cvLoadImage(charFileName);
+    //matImg = imread(charFileName);
+    //matImage = imread(charFileName);
+
     qimgNew = QImage((const unsigned char*)iplImg->imageData,iplImg->width,iplImg->height,QImage::Format_RGB888).rgbSwapped();
     currentImage = qimgNew;
-**/
-    //pas sur que ça fonctionne
-    ui->lblImage->setPixmap(QPixmap::fromImage(this->currentImage));
+
+    ui->lblImage->setPixmap(QPixmap::fromImage(qimgNew));
 }
 
 void MainWindow::on_saveImageButton_clicked()
 {
-    manage.SaveImage(this->currentImage);
+    //currentImage.save(fileName);
+    //QImageWriter writer("outimage.png", "png");
+    // writer.setText("Author", "John Smith");
+    // writer.write(currentImage);
+    QImageWriter write(fileName);
+    write.write(currentImage);
 }
 
 
 
 void MainWindow::on_BlurryRadioButton_clicked()
 {
-    filtreGris = NiveauGris::NiveauGris();
-    this->currentImage = filtreGris.traitementImage(this->currentImage);
+    ui->lblImage->clear();
 
-    ui->lblImage->setPixmap(manage.MatToQPixmap(this->currentImage));
+    IplImage *imgGray = cvLoadImage(fileName.toLocal8Bit().data(), CV_LOAD_IMAGE_GRAYSCALE);
+
+    qimgGray = QImage((const unsigned char*)imgGray->imageData,imgGray->width,imgGray->height,QImage::Format_Indexed8);
+
+    qimgGray.setPixel(0,0,qRgb(0,0,0));
+    currentImage = qimgGray;
+
+    ui->lblImage->setPixmap(QPixmap::fromImage(qimgGray));
 }
 
 
@@ -58,10 +68,55 @@ void MainWindow::on_BlurryRadioButton_clicked()
 
 void MainWindow::on_effet2_clicked()
 {
-    this->filtreNegatif = Negatif::Negatif();
-    this->currentImage = filtreNegatif.traitementImage(this->currentImage);
-    ui->lblImage->setPixmap(manage.MatToQPixmap(this->currentImage));
 
+   /* ui->lblImage->clear();
+
+     IplImage* img = cvLoadImage(fileName.toLocal8Bit().data());
+     cv::namedWindow("Image",CV_WINDOW_AUTOSIZE);
+
+     //Dilate Effect
+     cvNot(img, img);
+     cv::namedWindow("DilateEffect", CV_WINDOW_AUTOSIZE);
+     qimgGray = QImage((const unsigned char*)img->imageData,img->width,img->height,QImage::Format_Indexed8);
+
+     currentImage = qimgGray;
+
+     ui->lblImage->setPixmap(QPixmap::fromImage(qimgGray));*/
+
+
+    // original
+//    IplImage* img = cvLoadImage(fileName.toLocal8Bit().data());
+//    cv::namedWindow("Image", CV_WINDOW_AUTOSIZE);
+//    cvShowImage("Image", img);
+
+//    //Negative Effect
+//    cvNot(img, img);
+//    cv::namedWindow("NegativeEffect", CV_WINDOW_AUTOSIZE);
+//    cvShowImage("NegativeEffect", img);
+
+
+    //my test
+    cv::Mat image = cv::imread(fileName.toLocal8Bit().data(), CV_LOAD_IMAGE_COLOR);
+
+
+    cv::flip(image, image, 1);
+
+    //cvtColor(image, image,CV_BGR2RGB);
+    //qimgGray = QImage((const unsigned char*)image.data,image.cols,image.rows, image.step,QImage::Format_RGB888);
+
+    //qimgGray.setPixel(0,0,qRgb(0,0,0));
+    currentImage = qimgGray;
+
+    ui->lblImage->setPixmap(QPixmap::fromImage(Mat2QImage(image)));
+
+}
+
+QImage Mat2QImage(cv::Mat const& src)
+{
+     cv::Mat temp(src.cols,src.rows,src.type());
+     cvtColor(src, temp,CV_BGR2RGB);
+     QImage dest= QImage((uchar*) temp.data, temp.cols, temp.rows, temp.step, QImage::Format_RGB888);
+     return dest;
 }
 
 void MainWindow::on_effet3_clicked()
@@ -72,8 +127,8 @@ void MainWindow::on_effet3_clicked()
     cvShowImage("Image", img);
 
     //Negative Effect
-    cvErode(img, img, 0, 2);
-    cv::namedWindow("NegativeEffect", CV_WINDOW_AUTOSIZE);
+cvErode(img, img, 0, 2);
+cv::namedWindow("NegativeEffect", CV_WINDOW_AUTOSIZE);
     cvShowImage("NegativeEffect", img);
 
 
--- mainwindow.h
+++ mainwindow.h
@@ -2,9 +2,6 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-#include "imagemanager.h"
-#include "niveaugris.h"
-#include "negatif.h"
 #include "opencv2/imgproc.hpp"
 #include "opencv2/imgcodecs.hpp"
 #include "opencv2/highgui.hpp"
@@ -20,6 +17,7 @@ class MainWindow : public QMainWindow
 
 public:
     explicit MainWindow(QWidget *parent = 0);
+    QImage Mat2QImage(cv::Mat const& src);
     ~MainWindow();
 
 private slots:
@@ -29,30 +27,22 @@ private slots:
 
     void on_BlurryRadioButton_clicked();
 
-
     void on_effet2_clicked();
 
     void on_effet3_clicked();
 
     void on_effet4_clicked();
 
+
 private:
     Ui::MainWindow *ui;
-
-    cv::Mat currentImage;
     QString fileName;
-
-    ImageManager manage;
-    NiveauGris filtreGris;
-    Negatif filtreNegatif;
-
-
-    /*
     IplImage *iplImg;
     char* charFileName;
     QImage qimgNew;
     QImage qimgGray;
-    QImage currentImage;*/
+    QImage currentImage;
+    cv::Mat matImage;
 };
 
 #endif // MAINWINDOW_H
